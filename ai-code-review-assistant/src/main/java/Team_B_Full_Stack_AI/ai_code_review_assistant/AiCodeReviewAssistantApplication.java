a0e0cfb9d80347b3d18acbd0cc4b6561927a2ab1
Kashyap <piyush.d.kashyap@accenture.com>
2026-06-18 18:49:29 +0530

    Initial project setup with folder structure

---
+++ ai-code-review-assistant/src/main/java/Team_B_Full_Stack_AI/ai_code_review_assistant/AiCodeReviewAssistantApplication.java
@@ -0,0 +1,13 @@
+package Team_B_Full_Stack_AI.ai_code_review_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeReviewAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCodeReviewAssistantApplication.class, args);
+	}
+
+}
